ee091d0e1d604b1c3a178a47bf39cfb0f7256d72
vasyoid <vasyoid@gmail.com>
2017-11-08 08:28:32 +0300

    added some comments

--- Fp/src/main/java/net/netau/vasyoid/Collections.java
+++ Fp/src/main/java/net/netau/vasyoid/Collections.java
@@ -1,13 +1,25 @@
 package net.netau.vasyoid;
 
-import org.jetbrains.annotations.NotNull;
-
 import static java.util.Collections.reverse;
 
+import org.jetbrains.annotations.NotNull;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The class implements various methods to work with Iterable collections
+ * using functional style.
+ */
 public class Collections {
+
+    /**
+     *
+     * @param function
+     * @param arguments
+     * @param <A>
+     * @param <R>
+     * @return
+     */
     @NotNull
     public static <A, R> List<R> map(@NotNull Function1<? super A, ? extends R> function,
                                      @NotNull Iterable<? extends A> arguments) {
@@ -54,11 +66,11 @@ public class Collections {
         }
         return result;
     }
-    
+
     public static <R, A> R foldr(@NotNull Function2<? super A, ? super R, ? extends R> function,
                                  R init, @NotNull Iterable<? extends A> arguments) {
         List<? extends A> list = map((x -> x), arguments);
         reverse(list);
-        return foldl(function.swapArguments(), init, list);
+        return foldl(function.flip(), init, list);
     }
 }
