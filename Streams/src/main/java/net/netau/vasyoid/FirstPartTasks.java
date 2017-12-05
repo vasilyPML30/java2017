81a89769698c26c75b76c0e78e48b27741303e2d
vasyoid <vasyoid@gmail.com>
2017-12-05 22:25:10 +0300

    removed unused import statement

--- Streams/src/main/java/net/netau/vasyoid/FirstPartTasks.java
+++ Streams/src/main/java/net/netau/vasyoid/FirstPartTasks.java
@@ -4,7 +4,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
