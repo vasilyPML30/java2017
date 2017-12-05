af441be4cafc20734309a32364a401c8bb86cfb8
vasyoid <vasyoid@gmail.com>
2017-12-05 10:40:42 +0300

    first part tasks done

---
+++ Streams/src/main/java/net/netau/vasyoid/Artist.java
@@ -0,0 +1,14 @@
+package net.netau.vasyoid;
+
+public class Artist {
+
+    private final String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
