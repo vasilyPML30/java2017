af9a58f7e2894722288b5bda273993a4bc9d9ad9
vasyoid <vasyoid@gmail.com>
2017-12-11 23:16:41 +0300

    reflector initial commit

---
+++ Reflector/src/main/java/net/netau/vasyoid/Reflector.java
@@ -0,0 +1,11 @@
+package net.netau.vasyoid;
+
+import java.io.PrintWriter;
+
+public class Reflector {
+    public void printStructure(Class<?> someClass) {
+        try (PrintWriter out = new PrintWriter(...)) {
+
+        }
+    }
+}
