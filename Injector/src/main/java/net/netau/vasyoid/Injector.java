2c845c51f7f986686d9333d82c53a62621fe60e4
vasyoid <vasyoid@gmail.com>
2017-12-13 16:09:58 +0300

    test2 initial commit

---
+++ Injector/src/main/java/net/netau/vasyoid/Injector.java
@@ -0,0 +1,142 @@
+package net.netau.vasyoid;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+
+public class Injector {
+
+    @NotNull
+    private static Object getInstance(@NotNull Class classToInstantiate,
+                                      @NotNull HashMap instances)
+                                        throws ImplementationNotFoundException {
+        if (instances.containsKey(classToInstantiate)) {
+            return instances.get(classToInstantiate);
+        }
+        Constructor constructor = classToInstantiate.getConstructors()[0];
+        Object[] parameters = Arrays.stream(constructor.getParameterTypes())
+                .map((Function<Class, Object>) instances::get).toArray();
+        try {
+            return classToInstantiate.getConstructors()[0].newInstance(parameters);
+        } catch (Exception e) {
+            throw new ImplementationNotFoundException();
+        }
+    }
+
+    @NotNull
+    private static List<Class<?>> getInterfaceImplementations(@NotNull Class<?> interfaceClass,
+                                                  @NotNull Iterable<Class<?>> implementations) {
+        List<Class<?>> result = new ArrayList<>();
+        for (Class<?> implementation : implementations) {
+            for (Class<?> tInterface : implementation.getInterfaces()) {
+                if (tInterface == interfaceClass) {
+                    result.add(implementation);
+                }
+            }
+        }
+        return result;
+    }
+
+    @NotNull
+    private static List<Class<?>> getAbstractImplementations(@NotNull Class<?> abstractClass,
+                                                 @NotNull Iterable<Class<?>> implementations) {
+        List<Class<?>> result = new ArrayList<>();
+        for (Class<?> implementation : implementations) {
+            if (implementation.getSuperclass() == abstractClass) {
+                result.add(implementation);
+            }
+        }
+        return result;
+    }
+
+    private static void initializeCondidates(@NotNull List<Class<?>> condidates,
+                                             @NotNull Iterable<Class<?>> implementations,
+                                             @NotNull HashMap<Class, Object> instances)
+                                                                        throws Exception {
+        if (condidates.isEmpty()) {
+            throw new ImplementationNotFoundException();
+        } else if (condidates.size() > 1) {
+            throw new AmbigiousImplementationException();
+        } else {
+            initializeRecursively(condidates.get(0), implementations, instances);
+        }
+    }
+
+    @NotNull
+    private static Object initializeRecursively(@NotNull Class classToInitialize,
+                                                @NotNull Iterable<Class<?>> implementations,
+                                                @NotNull HashMap<Class, Object> instances)
+                                                                            throws Exception {
+        if (instances.containsKey(classToInitialize)) {
+            Object result = instances.get(classToInitialize);
+            if (result == null) {
+                throw new InjectionCycleException();
+            }
+            return result;
+        }
+        instances.put(classToInitialize, null);
+        Constructor constructor = classToInitialize.getConstructors()[0];
+        for (Class<?> parameterType : constructor.getParameterTypes()) {
+            if (!instances.containsKey(parameterType)) {
+                for (Class<?> implementation : implementations) {
+                    if (parameterType.isInterface()) {
+                        List<Class<?>> condidates = getInterfaceImplementations(parameterType,
+                                                                      implementations);
+                        initializeCondidates(condidates, implementations, instances);
+                    } else if (Modifier.isAbstract(parameterType.getModifiers())) {
+                        List<Class<?>> condidates = getAbstractImplementations(parameterType,
+                                                                     implementations);
+                        initializeCondidates(condidates, implementations, instances);
+                    } else {
+                        initializeRecursively(implementation, implementations, instances);
+                    }
+                }
+            }
+        }
+        Object result = getInstance(classToInitialize, instances);
+        instances.put(classToInitialize, result);
+        return result;
+    }
+
+    /**
+     * Initialises the class of the given name and returns its instance.
+     * @param rootClassName class to instantiate.
+     * @param implementations available classes for dependencies resolution.
+     * @return instantiated class.
+     * @throws Exception when could not resolve dependencies.
+     */
+    @NotNull
+    public static Object initialize(@NotNull String rootClassName,
+                                    @NotNull Iterable<Class<?>> implementations)
+                                                                throws Exception {
+        return initializeRecursively(Class.forName(rootClassName),
+                                     implementations, new HashMap<>());
+    }
+
+    /**
+     * Exception thrown when at least one of dependencies has at least two implementations.
+     */
+    public static class AmbigiousImplementationException extends Exception {
+
+    }
+
+    /**
+     * Exception thrown when at least one of dependencies does not have any implementations.
+     */
+    public static class ImplementationNotFoundException extends Exception {
+
+    }
+
+    /**
+     * Exception thrown when some dependencies are cyclic.
+     */
+    public static class InjectionCycleException extends Exception {
+
+    }
+}
