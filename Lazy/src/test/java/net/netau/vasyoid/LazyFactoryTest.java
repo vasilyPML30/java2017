7f92ac10092d8e9c809bb4af81c04f4df92a5594
vasyoid <vasyoid@gmail.com>
2018-03-06 17:43:39 +0300

    added multithread tests

--- Lazy/src/test/java/net/netau/vasyoid/LazyFactoryTest.java
+++ Lazy/src/test/java/net/netau/vasyoid/LazyFactoryTest.java
@@ -8,47 +8,86 @@ import static org.junit.Assert.*;
 
 public class LazyFactoryTest {
 
+    private static int TEST_NUM = 100;
+
+    private void runThreads(Runnable task) throws Exception {
+        Thread[] threads = new Thread[TEST_NUM];
+        for (int i = 0; i < TEST_NUM; i++) {
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
+        for (int i = 0; i < TEST_NUM; i++) {
+            threads[i].join();
+        }
+    }
+
     @Test
     public void multipleComputationsAssertCheck() throws Exception {
         Lazy singleLazyCustom = LazyFactory.createSingleThreadLazy(new CustomSupplier());
         Lazy multiLazyCustom = LazyFactory.createMultiThreadLazy(new CustomSupplier());
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < TEST_NUM; i++) {
             singleLazyCustom.get();
             multiLazyCustom.get();
         }
     }
 
     @Test
+    public void multiThreadMultipleComputationsAssertCheck() throws Exception {
+        Lazy lazyCustom = LazyFactory.createMultiThreadLazy(new CustomSupplier());
+        runThreads(lazyCustom::get);
+    }
+
+    @Test
     public void multipleGetsReturnTheSameNewObject() throws Exception {
         Lazy singleLazyObject = LazyFactory.createSingleThreadLazy(Object::new);
-        Lazy multiLazyObject = LazyFactory.createSingleThreadLazy(Object::new);
+        Lazy multiLazyObject = LazyFactory.createMultiThreadLazy(Object::new);
         Object singleObject = singleLazyObject.get();
         Object multiObject = multiLazyObject.get();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < TEST_NUM; i++) {
             assertTrue(singleObject == singleLazyObject.get());
             assertTrue(multiObject == multiLazyObject.get());
         }
     }
 
     @Test
+    public void multiThreadMultipleGetsReturnTheSameNewObject() throws Exception {
+        Lazy lazyObject = LazyFactory.createMultiThreadLazy(Object::new);
+        Object object = lazyObject.get();
+        runThreads(() -> assertTrue(object == lazyObject.get()));
+    }
+
+    @Test
     public void multipleGetsReturnTheSameOldObject() throws Exception {
         Object object = new Object();
         Lazy singleLazyObject = LazyFactory.createSingleThreadLazy(() -> object);
-        Lazy multiLazyObject = LazyFactory.createSingleThreadLazy(() -> object);
-        for (int i = 0; i < 10; i++) {
+        Lazy multiLazyObject = LazyFactory.createMultiThreadLazy(() -> object);
+        for (int i = 0; i < TEST_NUM; i++) {
             assertTrue(object == singleLazyObject.get());
             assertTrue(object == multiLazyObject.get());
         }
     }
 
     @Test
+    public void multiThreadMultipleGetsReturnTheSameOldObject() throws Exception {
+        Object object = new Object();
+        Lazy lazyObject = LazyFactory.createMultiThreadLazy(() -> object);
+        runThreads(() -> assertTrue(object == lazyObject.get()));
+    }
+
+    @Test
     public void nullObject() throws Exception {
         Lazy singleLazyNull = LazyFactory.createSingleThreadLazy(() -> null);
-        Lazy multiLazyNull = LazyFactory.createSingleThreadLazy(() -> null);
-        for (int i = 0; i < 10; i++) {
+        Lazy multiLazyNull = LazyFactory.createMultiThreadLazy(() -> null);
+        for (int i = 0; i < TEST_NUM; i++) {
             assertNull(singleLazyNull.get());
             assertNull(multiLazyNull.get());
         }
+    }
+
+    @Test
+    public void multiThreadNullObject() throws Exception {
+        Lazy lazyNull = LazyFactory.createMultiThreadLazy(() -> null);
+        runThreads(() -> assertNull(lazyNull.get()));
     }
 
     private static class CustomSupplier implements Supplier<Object> {
